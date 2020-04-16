674514dac8cf689daa70d2ed43ec5f5c5fe7a8b6
Bar Horing Amir <bar.apple@gmail.com>
2020-04-16 21:54:40 +0300

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const listRepos = async (username) => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
+  /*
+
+const listRepos = async (username) => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
+*/
+
+  const markup = repos
+    .map(
+      (repo) =>
+        `<li>
+      <a href="${repo.html_url}">${repo.name}</a>
+      (⭐ ${repo.stargazers_count})
+    </li>`
+    )
+    .join("");
+
+  console.log(markup);
+
+  const content = document.getElementById("content");
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos("barhoring");
