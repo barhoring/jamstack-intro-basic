12c8ff229939f4523df19afb83f6d0b01a74d0fc
Bar Horing Amir <bar.apple@gmail.com>
2020-04-20 22:12:34 +0300

    handle input of name field

--- contact/src/components/form.js
+++ contact/src/components/form.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useReducer } from "react";
 import styles from "./form.module.css";
 
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  subject: "",
+  body: "",
+};
+
+// To spare the pain of creating 4 different fuctions to handle the input
+// we use a reducer function
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "updateFieldValue":
+      return { ...state, [action.field]: action.value };
+
+    default:
+      return INITIAL_STATE;
+  }
+};
+
 const Form = () => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  // `updateFieldValue will be set in action.type in case of input change`
+  const updateFieldValue = (field) => (event) => {
+    dispatch({ type: "updateFieldValue", field, value: event.target.value });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <label className={styles.label}>
         Name
-        <input className={styles.input} type="text" name="name" />
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={updateFieldValue("name")}
+        />
       </label>
       <label className={styles.label}>
         Email
